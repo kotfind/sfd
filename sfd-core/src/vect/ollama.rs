7f7f5b65166a35f2db61497b938861d675535fdd
kotfind <kotfind@yandex.ru>
2026-06-18 00:15:01 +0300

    fix ollama

--- sfd-core/src/vect/ollama.rs
+++ sfd-core/src/vect/ollama.rs
@@ -7,36 +7,41 @@ use crate::{config::spec::Config, models::embedding::Embedding};
 use super::error::Error;
 
 #[derive(Serialize)]
-struct EmbeddingRequest<'a> {
+struct EmbedRequest<'a> {
     model: &'a str,
-    prompt: &'a str,
+    input: &'a str,
 }
 
 #[derive(Deserialize)]
-struct EmbeddingResponse {
-    embedding: Vec<f32>,
+struct EmbedResponse {
+    embeddings: Vec<Vec<f32>>,
 }
 
 /// Calls Ollama embedding model.
 ///
 /// Use on already-prepared text (see [super::prepare::prepare]).
 pub async fn embed_prepared(
-    prompt: &str,
+    text: &str,
     config: &Config,
     client: &Client,
 ) -> Result<Embedding, Error> {
     let url = Url::parse(&config.vect.ollama.url)
         .expect("invalid ollama base url")
-        .join("api/embeddings")
-        .expect("invalid ollama embeddings endpoint");
+        .join("api/embed")
+        .expect("invalid ollama embed endpoint");
     let resp = client
         .post(url)
-        .json(&EmbeddingRequest {
+        .json(&EmbedRequest {
             model: &config.vect.ollama.model,
-            prompt,
+            input: text,
         })
         .send()
         .await?;
-    let data: EmbeddingResponse = resp.error_for_status()?.json().await?;
-    Ok(Embedding::new(data.embedding))
+    let data: EmbedResponse = resp.error_for_status()?.json().await?;
+    Ok(Embedding::new(
+        data.embeddings
+            .into_iter()
+            .next()
+            .expect("expected exactly one embedding"),
+    ))
 }
